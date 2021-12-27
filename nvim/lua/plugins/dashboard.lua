830e3c6669fbd6cbc60e31d446155dcd894952d4
Barish Chandran B <barishcb@gmail.com>
2021-12-27 12:43:57 +1100

    - Updated keymaps for dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,2 +1,12 @@
-vim.g.dashboard_default_executive = 'fzf'
+local map = vim.api.nvim_set_keymap
+
 vim.g.dashboard_default_executive = 'telescope'
+
+map('n', '<Leader>ss', ':<C-u>SessionSave<CR>', {})
+map('n', '<Leader>sl', ':<C-u>SessionLoad<CR>', {})
+map('n', '<Leader>fh', ':DashboardFindHistory<CR>', {noremap = true, silent=true})
+map('n', '<Leader>ff', ':DashboardFindFile<CR>', {noremap = true, silent=true})
+map('n', '<Leader>tc', ':DashboardChangeColorscheme<CR>', {noremap = true, silent=true})
+map('n', '<Leader>fa', ':DashboardFindWord<CR>', {noremap = true, silent=true})
+map('n', '<Leader>fb', ':DashboardJumpMark<CR>', {noremap = true, silent=true})
+map('n', '<Leader>cn', ':DashboardNewFile<CR>', {noremap = true, silent=true})
