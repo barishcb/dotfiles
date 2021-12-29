902b16dde7c5e2851ffc8cb763a18ed9af1f19dc
Barish Chandran B <barishcb@gmail.com>
2021-12-29 21:53:26 +1100

    - Update dashboard config

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -2,6 +2,8 @@ local map = vim.api.nvim_set_keymap
 
 vim.g.dashboard_default_executive = 'telescope'
 
+vim.api.nvim_exec([[ autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2 ]], false)
+
 map('n', '<Leader>ss', ':<C-u>SessionSave<CR>', {})
 map('n', '<Leader>sl', ':<C-u>SessionLoad<CR>', {})
 map('n', '<Leader>fh', ':DashboardFindHistory<CR>', {noremap = true, silent=true})
